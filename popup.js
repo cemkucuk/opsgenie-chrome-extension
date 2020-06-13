88637b3cb1c7b753cdbd6bce61abc073b8c385d6
Cem Küçük <ckucuk@atlassian.com>
2020-06-13 12:51:01 +0300

    queried link for Opsgenie alerts page shortcuts and refactored popup page view with adding scroll

--- popup.js
+++ popup.js
@@ -1,22 +1,29 @@
 var rowText = '<td>x{count}</td><td><span class="ak-lozenge ak-lozenge__appearance-default-bold {priority}-bg">{priority}</span></td><td>{message}</td>'
 
 chrome.runtime.sendMessage({}, function (response) {
+  console.log(response)
   if (response.status !== "success") {
     document.getElementById("info-text").innerHTML = '<p style="color:#BF2600">' + response.reason + "</p>"
   } else {
-    console.log(response)
-    var infoText = document.getElementById("info-text")
-    infoText.style.removeProperty("padding")
-    infoText.innerHTML = "<i>Last updated <b>@ " + response.time + "</b></i>" + "<a style=\"float: right;\" href=\"https://app.opsgenie.com\" target=\"_blank\"> see all alerts↗ </a>"
     var table = document.getElementById("alert-list");
-    response.data.forEach(function (alert, i) {
-      var row = table.insertRow(i + 1);
-      row.setAttribute("id", alert.id)
-      row.innerHTML = rowText
-        .replace("{message}", alert.message)
-        .replace("{count}", alert.count)
-        .replace(/{priority}/g, alert.priority)
-    });
+    if(response.data.length > 0) {
+      var infoText = document.getElementById("info-text")
+      infoText.style.removeProperty("padding")
+      infoText.innerHTML = "<i>Last updated @ " + response.time + "</i>" + "<a style=\"float: right;\" href=\"" + response.ogUrl + "\" target=\"_blank\"> see all alerts↗ </a>"
+      response.data.forEach(function (alert, i) {
+        var row = table.insertRow(i + 1);
+        row.setAttribute("id", alert.id)
+        row.innerHTML = rowText
+          .replace("{message}", alert.message)
+          .replace("{count}", alert.count)
+          .replace(/{priority}/g, alert.priority)
+      });
+    } else {
+      var infoText = document.getElementById("info-text")
+      infoText.style.removeProperty("padding")
+      infoText.innerHTML = "<i>Last updated @ " + response.time + "</i>" + "<a style=\"float: right;\" href=\"" + response.ogUrl + "\" target=\"_blank\"> see all alerts↗ </a>" +
+      "<p style=\"text-align:center\"> There are no alerts.  🎉</p>"
+      }
   }
 });
 
